7b9853f0e2b6d9b36ae06d352ca52dcf07773734
bracesproul <braceasproul@gmail.com>
2025-01-26 15:39:20 -0800

    feat: example repo for using Agent Inbox with langgraph.js

--- src/agent/graph.ts
+++ src/agent/graph.ts
@@ -1,104 +1,82 @@
-/**
- * Starter LangGraph.js Template
- * Make this code your own!
- */
-import { StateGraph } from "@langchain/langgraph";
+import { interrupt, START, StateGraph } from "@langchain/langgraph";
 import { RunnableConfig } from "@langchain/core/runnables";
 import { StateAnnotation } from "./state.js";
+import {
+  ActionRequest,
+  HumanInterruptConfig,
+  HumanInterrupt,
+  HumanResponse,
+} from "@langchain/langgraph/prebuilt";
 
 /**
- * Define a node, these do the work of the graph and should have most of the logic.
- * Must return a subset of the properties set in StateAnnotation.
- * @param state The current state of the graph.
- * @param config Extra parameters passed into the state graph.
- * @returns Some subset of parameters of the graph state, used to update the state
- * for the edges and nodes executed next.
+ * Call the interrupt function to pause the graph and handle user interaction.
+ * Once resumed, it will log the type of action which was returned from
+ * the interrupt function.
  */
-const callModel = async (
-  state: typeof StateAnnotation.State,
+async function humanNode(
+  _state: typeof StateAnnotation.State,
   _config: RunnableConfig,
-): Promise<typeof StateAnnotation.Update> => {
-  /**
-   * Do some work... (e.g. call an LLM)
-   * For example, with LangChain you could do something like:
-   *
-   * ```bash
-   * $ npm i @langchain/anthropic
-   * ```
-   *
-   * ```ts
-   * import { ChatAnthropic } from "@langchain/anthropic";
-   * const model = new ChatAnthropic({
-   *   model: "claude-3-5-sonnet-20240620",
-   *   apiKey: process.env.ANTHROPIC_API_KEY,
-   * });
-   * const res = await model.invoke(state.messages);
-   * ```
-   *
-   * Or, with an SDK directly:
-   *
-   * ```bash
-   * $ npm i openai
-   * ```
-   *
-   * ```ts
-   * import OpenAI from "openai";
-   * const openai = new OpenAI({
-   *   apiKey: process.env.OPENAI_API_KEY,
-   * });
-   *
-   * const chatCompletion = await openai.chat.completions.create({
-   *   messages: [{
-   *     role: state.messages[0]._getType(),
-   *     content: state.messages[0].content,
-   *   }],
-   *   model: "gpt-4o-mini",
-   * });
-   * ```
-   */
-  console.log("Current state:", state);
-  return {
-    messages: [
-      {
-        role: "assistant",
-        content: `Hi there! How are you?`,
-      },
-    ],
+): Promise<Partial<typeof StateAnnotation.Update>> {
+  // Define the interrupt request
+  const actionRequest: ActionRequest = {
+    action: "Confirm Joke",
+    args: { joke: "What did the engineer say to the manager?" },
   };
-};
 
-/**
- * Routing function: Determines whether to continue research or end the builder.
- * This function decides if the gathered information is satisfactory or if more research is needed.
- *
- * @param state - The current state of the research builder
- * @returns Either "callModel" to continue research or END to finish the builder
- */
-export const route = (
-  state: typeof StateAnnotation.State,
-): "__end__" | "callModel" => {
-  if (state.messages.length > 0) {
-    return "__end__";
+  const interruptConfig: HumanInterruptConfig = {
+    allow_ignore: true, // Allow the user to `ignore` the interrupt
+    allow_respond: true, // Allow the user to `respond` to the interrupt
+    allow_edit: true, // Allow the user to `edit` the interrupt's args
+    allow_accept: true, // Allow the user to `accept` the interrupt's args
+  };
+
+  // The description will be rendered as markdown in the UI
+  const description = [
+    "# Confirm Joke",
+    "Please carefully examine the joke, and decide if you want to accept, edit, or ignore the joke.",
+    "If you accept, the joke will be added to the conversation.",
+    "If you edit and submit, the edited joke will be added to the conversation.",
+    "If you ignore, the conversation will continue without adding the joke.",
+    "If you respond, the response will be used to generate a new joke.",
+  ].join("\n");
+
+  const request: HumanInterrupt = {
+    action_request: actionRequest,
+    config: interruptConfig,
+    description,
+  };
+
+  const humanResponse = interrupt<HumanInterrupt[], HumanResponse[]>([
+    request,
+  ])[0];
+
+  if (humanResponse.type === "response") {
+    const message = `User responded with: ${humanResponse.args}`;
+    return { interruptResponse: message };
+  } else if (humanResponse.type === "accept") {
+    const message = `User accepted with: ${JSON.stringify(humanResponse.args)}`;
+    return { interruptResponse: message };
+  } else if (humanResponse.type === "edit") {
+    const message = `User edited with: ${JSON.stringify(humanResponse.args)}`;
+    return { interruptResponse: message };
+  } else if (humanResponse.type === "ignore") {
+    const message = "User ignored interrupt.";
+    return { interruptResponse: message };
   }
-  // Loop back
-  return "callModel";
-};
 
-// Finally, create the graph itself.
-const builder = new StateGraph(StateAnnotation)
-  // Add the nodes to do the work.
-  // Chaining the nodes together in this way
-  // updates the types of the StateGraph instance
-  // so you have static type checking when it comes time
-  // to add the edges.
-  .addNode("callModel", callModel)
-  // Regular edges mean "always transition to node B after node A is done"
-  // The "__start__" and "__end__" nodes are "virtual" nodes that are always present
-  // and represent the beginning and end of the builder.
-  .addEdge("__start__", "callModel")
-  // Conditional edges optionally route to different nodes (or end)
-  .addConditionalEdges("callModel", route);
+  return {
+    interruptResponse:
+      "Unknown interrupt response type: " + JSON.stringify(humanResponse),
+  };
+}
 
-export const graph = builder.compile();
+// Define a new graph
+const workflow = new StateGraph(StateAnnotation)
+  .addNode("humanNode", humanNode)
+  .addEdge(START, "humanNode");
 
-graph.name = "New Agent";
+// Compile the workflow into an executable graph
+export const graph = workflow.compile();
+
+// This defines the custom name in LangSmith
+graph.name = "Agent Inbox Example";
